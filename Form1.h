7d1f48cb14d5678b459177f8029492c01a817269
ZofD <lukasz.kwasny@interia.pl>
2020-11-27 17:48:54 +0100

    dodanie prostego logowania

--- Form1.h
+++ Form1.h
@@ -35,9 +35,11 @@ namespace CppCLRWinformsProjekt {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ login;
+	private: System::Windows::Forms::TextBox^ haslo;
 	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 
@@ -55,8 +57,8 @@ namespace CppCLRWinformsProjekt {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->login = (gcnew System::Windows::Forms::TextBox());
+			this->haslo = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
@@ -71,21 +73,21 @@ namespace CppCLRWinformsProjekt {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
-			// textBox1
+			// login
 			// 
-			this->textBox1->Location = System::Drawing::Point(209, 126);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(213, 26);
-			this->textBox1->TabIndex = 1;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			this->login->Location = System::Drawing::Point(209, 126);
+			this->login->Name = L"login";
+			this->login->Size = System::Drawing::Size(213, 26);
+			this->login->TabIndex = 1;
+			this->login->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
 			// 
-			// textBox2
+			// haslo
 			// 
-			this->textBox2->Location = System::Drawing::Point(209, 196);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(213, 26);
-			this->textBox2->TabIndex = 2;
-			this->textBox2->TextChanged += gcnew System::EventHandler(this, &Form1::textBox2_TextChanged);
+			this->haslo->Location = System::Drawing::Point(209, 196);
+			this->haslo->Name = L"haslo";
+			this->haslo->Size = System::Drawing::Size(213, 26);
+			this->haslo->TabIndex = 2;
+			this->haslo->TextChanged += gcnew System::EventHandler(this, &Form1::textBox2_TextChanged);
 			// 
 			// label1
 			// 
@@ -114,8 +116,8 @@ namespace CppCLRWinformsProjekt {
 			this->ClientSize = System::Drawing::Size(622, 454);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->haslo);
+			this->Controls->Add(this->login);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
@@ -125,10 +127,13 @@ namespace CppCLRWinformsProjekt {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		this->Hide();
-		ProjektCPPWindowsForms::MyForm uzytkownik;
-		uzytkownik.ShowDialog();
-		this->Show();
+		if (this->login->Text->Length > 0 && this->haslo->Text->Length > 0)
+		{
+			this->Hide();
+			ProjektCPPWindowsForms::MyForm uzytkownik;
+			uzytkownik.ShowDialog();
+			this->Show();
+		}
 	}
 private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
--- MyForm.h
+++ MyForm.h
@@ -35,6 +35,12 @@ namespace ProjektCPPWindowsForms {
 			}
 		}
 	private: System::Windows::Forms::Label^ user;
+	private: System::Windows::Forms::TextBox^ szukaj;
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ListView^ listView1;
+	private: System::Windows::Forms::Button^ button2;
 	protected:
 
 	protected:
@@ -52,30 +58,98 @@ namespace ProjektCPPWindowsForms {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::Windows::Forms::ListViewItem^ listViewItem9 = (gcnew System::Windows::Forms::ListViewItem(L""));
+			System::Windows::Forms::ListViewItem^ listViewItem10 = (gcnew System::Windows::Forms::ListViewItem(L""));
+			System::Windows::Forms::ListViewItem^ listViewItem11 = (gcnew System::Windows::Forms::ListViewItem(L""));
+			System::Windows::Forms::ListViewItem^ listViewItem12 = (gcnew System::Windows::Forms::ListViewItem(L""));
 			this->user = (gcnew System::Windows::Forms::Label());
+			this->szukaj = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// user
 			// 
 			this->user->AutoSize = true;
-			this->user->Location = System::Drawing::Point(48, 30);
+			this->user->Location = System::Drawing::Point(49, 27);
 			this->user->Name = L"user";
 			this->user->Size = System::Drawing::Size(145, 20);
 			this->user->TabIndex = 0;
 			this->user->Text = L"nazwa uzytkownika";
+			// 
+			// szukaj
+			// 
+			this->szukaj->Location = System::Drawing::Point(725, 24);
+			this->szukaj->Name = L"szukaj";
+			this->szukaj->Size = System::Drawing::Size(452, 26);
+			this->szukaj->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(868, 10);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 20);
+			this->label1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(642, 18);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(77, 38);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Szukaj";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// listView1
+			// 
+			this->listView1->HideSelection = false;
+			listViewItem9->Tag = L"cup";
+			listViewItem10->Tag = L"mop";
+			listViewItem11->Tag = L"pop";
+			listViewItem12->Tag = L"op";
+			this->listView1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ListViewItem^  >(4) {
+				listViewItem9, listViewItem10,
+					listViewItem11, listViewItem12
+			});
+			this->listView1->Location = System::Drawing::Point(12, 69);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(1326, 675);
+			this->listView1->TabIndex = 4;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(1212, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(126, 50);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"Wyloguj";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1350, 756);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->listView1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->szukaj);
 			this->Controls->Add(this->user);
-			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Name = L"Panel klienta";
+			this->Text = L"Panel klienta";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Hide();
+}
+};
 }
